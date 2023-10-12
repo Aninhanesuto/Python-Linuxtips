43ee2289b669650aa8fa8bddddd278738796479c
AninhaAninha/ <101958736+Aninhanesuto@users.noreply.github.com>
2023-10-12 18:08:32 +0000

    Pending changes exported from your codespace

---
+++ 1.py
@@ -0,0 +1,11 @@
+##print("Bom Dia", end="&")
+#Pode usar aspas simples ou duplas.
+x = 10
+y = 12
+print("X =", x, sep="")
+print(1,2,3,4,5, sep="*")
+x1 = str(x)
+y1 = str(y)
+print("O valor de x é: {} e o valor de y é: {}".format(x1, y1))
+print(f"O valor de x é {x1}")
+print("Valor de x é {}". format(87))
